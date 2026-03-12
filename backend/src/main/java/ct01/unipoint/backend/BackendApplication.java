6762989bbf09d5964862450b38c61820ded727ff
NguyenTuKien <KienNT.B23CN465@stu.ptit.edu.vn>
2026-03-12 22:29:42 +0700

    Test

--- backend/src/main/java/ct01/unipoint/backend/BackendApplication.java
+++ backend/src/main/java/ct01/unipoint/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package ct01.sslink.backend;
+package ct01.unipoint.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
